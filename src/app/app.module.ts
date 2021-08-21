51ec59f82833df297bb98e8ebfca24952bddf138
akilan <selvamakilan1@gmail.com>
2021-08-21 15:19:45 +0530

    Fourth Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,14 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
+import { ButtonModule, RadioButtonModule } from '@syncfusion/ej2-angular-buttons';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { SidebarModule } from '@syncfusion/ej2-angular-navigations';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,MatToolbarModule,MatIconModule,MatButtonModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    imports: [SidebarModule, ButtonModule, RadioButtonModule, BrowserModule],
+    declarations: [AppComponent],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
